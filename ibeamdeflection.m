5a3ac51feb7f34eeb8e7abd37fff738e90ccdf36
ipokemy <81012052+ipokemy@users.noreply.github.com>
2021-06-23 10:19:16 +0800

    Update ibeamdeflection.m

--- ibeamdeflection.m
+++ ibeamdeflection.m
@@ -1,4 +1,5 @@
 % JEREMY CHOY (145923) 
+% To calculate vertical deflection of an I-beam
 
 clc, clear all, close all
 
@@ -11,11 +12,11 @@ t=8e-3;
 L=2; 
 x=0:0.1:L;
 
-% Inertia 
+% Inertia Function
 I=M(h,b,t,L,x); 
 
 % Vertical Deflection
-V=(P*x.^2).*(3*L-x)/(E*I);
+V=(P*x.^2).*(3*L-x)./(E*I);
 
 % Plot
 plot(x,V);
@@ -26,13 +27,15 @@ ylabel('vertical deflection (m)')
 
 % Inertia (Subroutine)
 function I=M(h,b,t,L,x)
+n=length(x);
+I=ones(1,n);
 
-    if (x>=0 & x<L/2) 
+for i = 1:n
+    if (x(i)>=0 && x(i)<L/2)
         w=150e-3;
-    else 
+    else
         w=90e-3;
     end
-    
-I=(w*h^3-(h-2*t)^3*(w-b))/12;
-
+    I(i)=(w*h^3-(h-2*t)^3*(w-b))/12;
+end
 end
